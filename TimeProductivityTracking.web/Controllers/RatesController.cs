96ce8729f1b4c4c588cfa857807e21242daf6ddf
Wiphawan Klinhom <X00204399@myTUDublin.ie>
2025-03-26 10:01:57 +0000

    Added Rate Create_Update_Delete

---
+++ TimeProductivityTracking.web/Controllers/RatesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TimeProductivityTracking.web.Data;
+using TimeProductivityTracking.web.Models;
+
+namespace TimeProductivityTracking.web.Controllers
+{
+    public class RatesController : Controller
+    {
+        private readonly ProductivitiesContext _context;
+
+        public RatesController(ProductivitiesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rates
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rates.ToListAsync());
+        }
+
+        // GET: Rates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rate = await _context.Rates
+                .FirstOrDefaultAsync(m => m.RateID == id);
+            if (rate == null)
+            {
+                return NotFound();
+            }
+
+            return View(rate);
+        }
+
+        // GET: Rates/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RateID,RateName,HourlyWage")] Rate rate)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rate);
+        }
+
+        // GET: Rates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rate = await _context.Rates.FindAsync(id);
+            if (rate == null)
+            {
+                return NotFound();
+            }
+            return View(rate);
+        }
+
+        // POST: Rates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RateID,RateName,HourlyWage")] Rate rate)
+        {
+            if (id != rate.RateID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RateExists(rate.RateID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rate);
+        }
+
+        // GET: Rates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rate = await _context.Rates
+                .FirstOrDefaultAsync(m => m.RateID == id);
+            if (rate == null)
+            {
+                return NotFound();
+            }
+
+            return View(rate);
+        }
+
+        // POST: Rates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rate = await _context.Rates.FindAsync(id);
+            if (rate != null)
+            {
+                _context.Rates.Remove(rate);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RateExists(int id)
+        {
+            return _context.Rates.Any(e => e.RateID == id);
+        }
+    }
+}
---
+++ TimeProductivityTracking.web/Migrations/20250324104608_MyMigrationUserInfo_Rate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeProductivityTracking.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyMigrationUserInfo_Rate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInfo_RateID",
+                table: "UserInfo",
+                column: "RateID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInfo_Rate_RateID",
+                table: "UserInfo",
+                column: "RateID",
+                principalTable: "Rate",
+                principalColumn: "RateID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInfo_Rate_RateID",
+                table: "UserInfo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserInfo_RateID",
+                table: "UserInfo");
+        }
+    }
+}
